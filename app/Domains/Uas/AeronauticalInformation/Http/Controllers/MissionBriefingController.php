56a9e5f96c04bab96cd44d04425ca7fb34d08a05
Mabonax <104203635+Mabonax@users.noreply.github.com>
2026-09-18 10:33:26 +0200

    TR-003 tenant-scope mission briefing operational API

--- app/Domains/Uas/AeronauticalInformation/Http/Controllers/MissionBriefingController.php
+++ app/Domains/Uas/AeronauticalInformation/Http/Controllers/MissionBriefingController.php
@@ -10,6 +10,7 @@ use App\Domains\Uas\AeronauticalInformation\Http\Requests\AcknowledgeBriefingReq
 use App\Domains\Uas\AeronauticalInformation\Http\Requests\GenerateBriefingRequest;
 use App\Domains\Uas\Api\Application\ApiResponse;
 use App\Domains\Uas\Missions\Domain\Models\UasMission;
+use App\Domains\Uas\Operators\Application\Queries\CurrentOperatorContext;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -18,6 +19,7 @@ class MissionBriefingController extends Controller
 {
     public function show(Request $request, UasMission $mission, MissionBriefing $query)
     {
+        $this->enforceApiTenant($request, $mission);
         $input = $request->validate(['revision' => 'nullable|integer|min:1']);
         $data = $query->execute($mission, $request->user(), isset($input['revision']) ? (int) $input['revision'] : null);
 
@@ -26,6 +28,7 @@ class MissionBriefingController extends Controller
 
     public function store(GenerateBriefingRequest $request, UasMission $mission, GenerateMissionBriefing $generate, MissionBriefing $query)
     {
+        $this->enforceApiTenant($request, $mission);
         $generate->execute($mission, $request->user(), $request->validated('aeronautical_context'));
 
         return $request->routeIs('api.v1.*') ? ApiResponse::success($query->execute($mission->refresh(), $request->user()), 'Briefing generated.', 201) : to_route('missions.briefing.show', $mission);
@@ -33,8 +36,18 @@ class MissionBriefingController extends Controller
 
     public function acknowledge(AcknowledgeBriefingRequest $request, UasMission $mission, MissionAeronauticalBriefing $briefing, AcknowledgeMissionBriefing $acknowledge, MissionBriefing $query)
     {
+        $this->enforceApiTenant($request, $mission);
         $acknowledge->execute($mission, $briefing, $request->user());
 
         return $request->routeIs('api.v1.*') ? ApiResponse::success($query->execute($mission, $request->user()), 'Briefing acknowledged.') : to_route('missions.briefing.show', $mission);
+    }
+    private function enforceApiTenant(Request $request, UasMission $mission): void
+    {
+        if (! $request->routeIs('api.v1.*')) {
+            return;
+        }
+
+        $operator = app(CurrentOperatorContext::class)->requireFromRequest($request);
+        abort_unless((int) $mission->uas_operator_id === (int) $operator->id, 404);
     }
 }
